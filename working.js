4eb3cbbff7c009d2a6473e2c43ad83cf6e37e9b1
Conner Gulley <71948090+CanisLupus81@users.noreply.github.com>
2021-01-04 14:43:00 -0600

    Addition to JavaScript!

---
+++ working.js
@@ -0,0 +1,5 @@
+document.getElementById("button").onclick = function() {
+  document.getElementById("hurt_feelings").innerHTML = "Thank you!";
+  document.getElementById("button").style.display = "none";
+}
+alert("Welcome to my website! Enjoy your stay!");
